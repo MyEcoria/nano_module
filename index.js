7984ab5ea1c1a263435cc8d9eb9c46abe778b2bb
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2023-01-10 22:00:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,193 +1,73 @@
-const https = require('https');
+const request = require('request');
 
-const postData = (url,data,apiKey) => {
-    let req = https.request({
-        host:url,
+module.exports = {
+  accountBalance: (apiKey, coin, account) => {
+    return new Promise((resolve, reject) => {
+      const options = {
         method: 'POST',
+        url: `https://nodes.myecoria.com/${coin}`,
         headers: {
-            'Content-Type': 'application/json',
-            'x-auth-key': apiKey
+          'x-auth-key': apiKey
+        },
+        json: {
+          action: 'account_balance',
+          account: account
         }
-    }, (res) => {
-        res.on('data', (d) => {
-            process.stdout.write(d);
-        });
+      };
+    
+      request(options, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(JSON.stringify(body));
+        }
+      });
     });
-
-    req.write(JSON.stringify(data));
-    req.end();
-
-    req.on('error', (error) => {
-        console.error(error);
+  },
+  accountBlockCount: (apiKey, coin, account) => {
+    return new Promise((resolve, reject) => {
+      const options = {
+        method: 'POST',
+        url: `https://nodes.myecoria.com/${coin}`,
+        headers: {
+          'x-auth-key': apiKey
+        },
+        json: {
+          action: 'account_block_count',
+          account: account
+        }
+      };
+    
+      request(options, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(JSON.stringify(body));
+        }
+      });
     });
-}
-
-exports.account_balance = (account, coin,apiKey) => {
-    let data = {
-        "action": "account_balance",
-        "account": account
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.account_block_count = (account, coin, apiKey) => {
-    let data = {
-        "action": "account_block_count",
-        "account": account
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.account_get = (key, coin,apiKey) => {
-    let data = {
-        "action": "account_get",
-        "key": key
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.account_info = (account, coin,apiKey) => {
-    let data = {
-        "action": "account_info",
-        "account": account
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.account_key = (account, coin, apiKey) => {
-    let data = {
-        "action": "account_key",
-        "account": account
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.account_representative = (account, coin,apiKey) => {
-    let data = {
-        "action": "account_representative",
-        "account": account
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.block_account = (hash, coin, apiKey) => {
-    let data = {
-        "action": "block_account",
-        "hash": hash
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-exports.block_count = (coin, apiKey) => {
-    let data = {
-        "action": "block_count"
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.block_info = (hash, json_block, coin, apiKey) => {
-    let data = {
-        "action": "block_info",
-        "hash": hash,
-        "json_block": json_block
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.delegators_count = (account, coin, apiKey) => {
-    let data = {
-        "action": "delegators_count",
-        "account": account
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.receivable = (account, count, coin, apiKey) => {
-    let data = {
-        "action": "receivable",
-        "account": account,
-        "count": count
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.account_create = (wallet, coin, apiKey) => {
-    let data = {
-        "action": "account_create",
-        "wallet": wallet
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.account_list = (wallet, coin, apiKey) => {
-    let data = {
-        "action": "account_list",
-        "wallet": wallet
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.account_representative_set = (wallet, account, representative, coin, apiKey) => {
-    let data = {
-        "action": "account_representative_set",
-        "wallet": wallet,
-        "account": account,
-        "representative": representative
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.receive = (wallet, account, block, coin, apiKey) => {
-    let data = {
-        "action": "receive",
-        "wallet": wallet,
-        "account": account,
-        "block": block
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.send = (wallet, source, destination, amount, id, coin, apiKey) => {
-    let data = {
-        "action": "senhttps://d",
-        "wallet": wallet,
-        "source": source,
-        "destination": destination,
-        "amount": amount,
-        "id": id
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.unchecked = (count, coin, apiKey) => {
-    let data = {
-        "action": "unchecked",
-        "count": count
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
-
-exports.validate_account_number = (account, coin, apiKey) => {
-    let data = {
-        "action": "validate_account_number",
-        "account": account
-    }
-    let url =`https://nodes.myecoria.com/${coin}`;
-    postData(url,data,apiKey);
-}
+  },
+  accountGet: (apiKey, coin, key) => {
+    return new Promise((resolve, reject) => {
+      const options = {
+        method: 'POST',
+        url: `https://nodes.myecoria.com/${coin}`,
+        headers: {
+          'x-auth-key': apiKey
+        },
+        json: {
+          action: 'account_get',
+          key: key
+        }
+      };
+    
+      request(options, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(JSON.stringify(body));
+        }
+      });
+    });
+  }
+};
